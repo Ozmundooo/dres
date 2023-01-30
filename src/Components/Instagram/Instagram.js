15413aedac98f6912a3424fba59f03ea28cfe3a7
Ozmundooo <osman.ali.dev@gmail.com>
2023-01-30 12:34:56 -0500

    removed ig part for now

--- src/Components/Instagram/Instagram.js
+++ src/Components/Instagram/Instagram.js
@@ -1,15 +1,14 @@
 import React from "react";
-import InstagramFeed  from 'react-ig-feed'
-import 'react-ig-feed/dist/index.css'
-import './Instagram.scss'
-function Instagram(){
-    
-    return(
-        <>  
-            <div className="Instagram">
+import InstagramFeed from "react-ig-feed";
+import "react-ig-feed/dist/index.css";
+import "./Instagram.scss";
+function Instagram() {
+  return (
+    <>
+      {/* <div className="Instagram">
                 <InstagramFeed token='IGQVJVME9NdXBrYnIzRXRwWi1jeUU5dUZAsdTZAWREFNWERFYy1BWlFfeU5QNkRSTjM2VksyQllILWQySGtfbV9RaWtqUWxMSV8tRkVBamNwSWRudUNfdWszaFA1Wko3cG96eGpsRi04OC1KYWpGZAkZASLQZDZD'  counter="3"/>
-            </div>
-        </>
-    )
+            </div> */}
+    </>
+  );
 }
-export default Instagram
+export default Instagram;
